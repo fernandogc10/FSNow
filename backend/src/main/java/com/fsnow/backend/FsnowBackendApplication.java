b5e0478450e730a5afb4e92695993bfb94261671
fernandogc10 <91064957+fernandogc10@users.noreply.github.com>
2024-12-19 14:01:08 +0100

    first commmit

---
+++ backend/src/main/java/com/fsnow/backend/FsnowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fsnow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsnowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsnowBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fsnow/backend/FsnowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsnow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsnowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
